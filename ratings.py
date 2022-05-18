e83df56f86a7aca1d9ccbdd76ba4650d6c828efc
Molli Budd <mbudd101288@gmail.com>
2022-05-17 23:17:42 -0500

    two different ways to get sorted info

--- ratings.py
+++ ratings.py
@@ -20,22 +20,26 @@ def restaurant_rating(file):
         
         restaurant_dict[restaurant] = rating 
 
-        sorted_restaurant = sorted(restaurant_dict.items())
+        # # 1
+        # sorted_restaurant = sorted(restaurant_dict.items())
+        
 
-    # print(sorted_restaurant)
+    # # 1 when you sort a dictionary it becomes a list of tupules(key, value) that you have to unpack
+    # for key, value in sorted_restaurant:
+    #     # rest is the key and rate is the value
+    #     print(f"{key} is rated {value}")
 
-    # when you sort a dictionary it becomes a list of tupules(key, value) that you have to unpack
-    for rest, rate in sorted_restaurant:
-        # rest is the key and rate is the value
-        print(f"{rest} is rated {rate}")
+         # 2
+        sorted_restaurant = sorted(restaurant_dict.keys())  
+    # # 2 could also not split restaurant_dict into items(tupules) and iterate through the sorted keys instead
+    for restaurant in sorted_restaurant:
+        if restaurant in restaurant_dict:
+            sorted_restaurant = restaurant
             
-    # # could also not split restaurant_dict into items(tupules) and iterate through the sorted keys instead
-    # for word in sorted_restaurant:
-    #     if word in restaurant_dict:
-    #         rest_s = word
-            
-    #         rate_s = restaurant_dict[word]
-    #         print(f"{rest_s} is rated {rate_s}")
+            sorted_rating = restaurant_dict[sorted_restaurant]
+            print(f"{sorted_restaurant} is rated {sorted_rating}")
+    
+
 
     # for restaurant in restaurant_dict:
     #     user_rest_choice = input("Pick a restaurant: ")
